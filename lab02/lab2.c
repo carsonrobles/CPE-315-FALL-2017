a28034f23a49020c55059268e1a4bd21a6e89b30
Carson Robles <carson.robles@gmail.com>
2017-09-27 15:33:39 -0700

    adding lab 2

---
+++ lab02/lab2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdint.h>
+
+unsigned int umultiply(unsigned int a, unsigned int b) {
+    unsigned int prod;
+
+    
+
+    return prod;
+}
+
+void part1(void) {
+    printf("=========Part 1==========\n");
+    printf("1a. a=0x0001, b=0x0001 c=0x%04x\n", umultiply(0x0001, 0x0001));
+    printf("1b. a=0x0001, b=0xFFFF c=0x%04x\n", umultiply(0x0001, 0x0001));
+    printf("1c. a=0x8000, b=0x0001 c=0x%04x\n", umultiply(0x0001, 0x0001));
+    printf("1d. a=0x4000, b=0x4000 c=0x%04x\n", umultiply(0x0001, 0x0001));
+    printf("1e. a=0x8000, b=0x8000 c=0x%04x\n", umultiply(0x0001, 0x0001));
+    printf("=========================\n\n");
+}
+
+void part2(void) {
+    printf("=========Part 2==========\n");
+
+    printf("=========================\n\n");
+}
+
+void part3(void) {
+    printf("=========Part 3==========\n");
+
+    printf("=========================\n\n");
+}
+
+void part4(void) {
+    printf("=========Part 4==========\n");
+
+    printf("=========================\n\n");
+}
+
+void part5(void) {
+    printf("=========Part 5==========\n");
+
+    printf("=========================\n\n");
+}
+
+void part6(void) {
+    printf("=========Part 6==========\n");
+
+    printf("=========================\n\n");
+}
+
+void part7(void) {
+    printf("=========Part 7==========\n"); 
+
+    printf("=========================\n\n");
+}
+
+int main(void) {
+    part1();
+    part2();
+    part3();
+    part4();
+    part5();
+    part6();
+    part7();
+
+    return 0;
+}
