34351f0ec021faac44f1aca6e0bce71cc0be9317
Carson Robles <carson.robles@gmail.com>
2017-10-18 01:57:01 -0700

    Work on bintohex

--- lab04/input.s
+++ lab04/input.s
@@ -14,13 +14,20 @@
 
 	.data
 hexstr:	.space	10
-tmpspc:	.space	1
 
 	.text
 main:	and	$a1, $a1, $zero		# clear $a1
 	la	$a1, hexstr
+	li	$a0, 0x12345678
 	
+	jal	bintohex
 	
+	li	$v0, 4
+	la	$a0, hexstr
+	
+	syscall
+	
+	b 	main
 
 # ------------------------------ #
 # bintohex
@@ -29,11 +36,56 @@ main:	and	$a1, $a1, $zero		# clear $a1
 # $a1: address to begin writing null terminated hex string
 # ------------------------------ #
 bintohex:
+	# push used saved registers on stack
+	addi 	$sp, $sp, -4
+	sw	$s0, 0($sp)
+	
+	addi 	$sp, $sp, -4
+	sw	$s1, 0($sp)
+	
+	addi 	$sp, $sp, -4
+	sw	$s2, 0($sp)
+	
+	addi 	$sp, $sp, -4
+	sw	$s3, 0($sp)
+
 	# init counter to decimal 10 and changeable address
-	addi	$t0, $zero, 0xa 
-	add	$t1, $a1, $zero
+	addi	$s0, $zero, 0xa		# counter = 10
+	addi	$s1, $a1,   0x28	# address = last address
+	addi 	$s2, $zero, 0x0		# current nybble
+	addi	$s3, $a0,   0x0		# vector to convert
+	
+	# write null character
+	sw	$zero, 0($s1)
 	
 	# iterate over $a0, 4 bits at a time
+bhloop:	addi	$s0, $s0, -1
+	addi	$s1, $s1, -4
+	
+	and 	$s2, $s3, 0xf
+	srl	$s3, $s3, 0x4
+	
+	addi	$s2, $s2, 0x30
+	
+	sw	$s2, 0($s1)
+	
+	bnez	$s0, bhloop
 	
 	# compute ASCII value
 	# store at address and update address
+	
+	# pop used saved registers
+	lw	$s3, 0($sp)
+	addi	$sp, $sp, 4
+	
+	lw	$s2, 0($sp)
+	addi	$sp, $sp, 4
+
+	lw	$s1, 0($sp)
+	addi	$sp, $sp, 4
+	
+	lw	$s0, 0($sp)
+	addi	$sp, $sp, 4
+	
+	# return from subroutine
+	jr	$ra
