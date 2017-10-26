fc28059305c5ec5dfc3d24bd826f89227f09aa59
Carson Robles <carson.robles@gmail.com>
2017-10-25 18:36:17 -0700

    Add R and J type instruction decodes

--- lab05/decode.c
+++ lab05/decode.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
+#include "decode.h"
+
 typedef unsigned int MIPS, *MIPS_PTR;
 
-void decode(MIPS instruction) {
-    unsigned short opcode = (instruction | 0) >> 24;
+void decode(MIPS bits) {
+    instruction instr;
 
-    if (opcode == 0) {
+    // clear instr
+    memset(&instr, 0, sizeof(instruction));
+
+    // get opcode from bits argument
+    instr.op = (instruction | 0) >> 24;
+
+    if (instr.op == 0) {
         // R
-    } else if (opcode == 2 || opcode == 3) {
+        instr.type  = R_INSTR;
+        instr.rs    = (bits >> 21) & 0x1f;
+        instr.rt    = (bits >> 16) & 0x1f;
+        instr.rd    = (bits >> 11) & 0x1f;
+        instr.shamt = (bits >>  6) & 0x1f;
+        instr.funct =  bits        & 0x3f;
+    } else if (instr.op == 2 || instr.op == 3) {
         // J
+        instr.type    = J_INSTR;
+        instr.wordind = bits & 0x3ffffff;
     } else {
         // I
     }
