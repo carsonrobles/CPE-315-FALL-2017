0727780eb9ca3fe1d3e55d08f3b43667d3233293
Dane Mortensen <mortensendane@gmail.com>
2017-10-25 18:36:11 -0700

    immediate portion of decode function

--- lab05/decode.c
+++ lab05/decode.c
@@ -2,14 +2,18 @@
 
 typedef unsigned int MIPS, *MIPS_PTR;
 
-void decode(MIPS instruction) {
-    unsigned short opcode = (instruction | 0) >> 24;
+void decode(MIPS bits) {
+    instruction instr;
 
+    instr.op = (bits | 0) >> 26;
     if (opcode == 0) {
         // R
     } else if (opcode == 2 || opcode == 3) {
         // J
-    } else {
-        // I
+    } else {            /* I */
+        instr.type = I_INSTR;
+        instr.rs = (bits & 0x3e00000) >> 21;
+        instr.rt = (bits & 0x1f0000) >> 16;
+        instr.imm = bits & 0xffff
     }
 }
