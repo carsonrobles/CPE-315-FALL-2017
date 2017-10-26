310edd475ba541e682267361589990be3157c965
Dane Mortensen <mortensendane@gmail.com>
2017-10-25 18:00:51 -0700

    decode function

--- lab05/decode.c
+++ lab05/decode.c
@@ -1,1 +1,15 @@
-// text
+#include <stdio.h>
+
+typedef unsigned int MIPS, *MIPS_PTR;
+
+void decode(MIPS instruction) {
+    unsigned short opcode = (instruction | 0) >> 24;
+
+    if (opcode == 0) {
+        // R
+    } else if (opcode == 2 || opcode == 3) {
+        // J
+    } else {
+        // I
+    }
+}
